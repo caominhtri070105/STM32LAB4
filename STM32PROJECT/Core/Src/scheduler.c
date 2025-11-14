9e3faa3e21e3e1ddb5e5ccdc6c6b7e8f9538e7d5
caominhtri070105 <caominhtri070105@gmail.com>
2025-11-15 00:51:03 +0700

    Update for Lab4

---
+++ STM32PROJECT/Core/Src/scheduler.c
@@ -0,0 +1,10 @@
+/*
+ * scheduler.c
+ *
+ *  Created on: Nov 15, 2025
+ *      Author: tri
+ */
+
+#include "scheduler.h"
+sTasks SCH_tasks_G[SCH_MAX_TASKS];
+uint8_t current_index_task=0;
--- STM32PROJECT/Debug/Core/Src/subdir.mk
+++ STM32PROJECT/Debug/Core/Src/subdir.mk
@@ -9,6 +9,7 @@ C_SRCS += \
 ../Core/Src/button.c \
 ../Core/Src/global.c \
 ../Core/Src/main.c \
+../Core/Src/scheduler.c \
 ../Core/Src/stm32f1xx_hal_msp.c \
 ../Core/Src/stm32f1xx_it.c \
 ../Core/Src/syscalls.c \
@@ -20,6 +21,7 @@ OBJS += \
 ./Core/Src/button.o \
 ./Core/Src/global.o \
 ./Core/Src/main.o \
+./Core/Src/scheduler.o \
 ./Core/Src/stm32f1xx_hal_msp.o \
 ./Core/Src/stm32f1xx_it.o \
 ./Core/Src/syscalls.o \
@@ -31,6 +33,7 @@ C_DEPS += \
 ./Core/Src/button.d \
 ./Core/Src/global.d \
 ./Core/Src/main.d \
+./Core/Src/scheduler.d \
 ./Core/Src/stm32f1xx_hal_msp.d \
 ./Core/Src/stm32f1xx_it.d \
 ./Core/Src/syscalls.d \
